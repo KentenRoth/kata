6e989f5acfc128b8805ce0f24fd2cfe67cdb90dd
Kent Roth <kentencroth@yahoo.com>
2019-06-10 22:22:29 -0500

    WIP: added wage output for Family A

--- babysitter.js
+++ babysitter.js
@@ -7,14 +7,42 @@ const hoursCheck = (start, leave) =>
 		? true
 		: false;
 
+// Checking to make sure leave time is not before start time
 const leaveBeforeStart = (start, leave) =>
 	hours.indexOf(start) <= hours.indexOf(leave) - 1 ? true : false;
 
+// Checking to make sure both hoursCheck and leaveBeforeStart are true.
 const workableHours = (start, leave) =>
 	hoursCheck(start, leave) && leaveBeforeStart(start, leave) ? true : false;
+
+// Family A - $15 hourly before 11pm, $20 hourly rest of night
+const familyA = (start, leave) => {
+	const startTime = hours.indexOf(start);
+	const leaveIndex = hours.indexOf(leave);
+	const leaveTime = hours.slice(0, leaveIndex);
+	let payment = 0;
+
+	if (workableHours(start, leave) !== true) {
+		return 'Please check the hours entered';
+	}
+
+	for (i = startTime; leaveTime.length > i; i++) {
+		if (i <= 5) {
+			payment += 15;
+		} else {
+			payment += 20;
+		}
+	}
+	return payment;
+};
+
+// Family B - $12 hourly before 10pm, $8 hourly between 10 - 12, $16 hourly after 12
+
+// Family C - $21 hourly before 9pm, $15 hourly after
 
 module.exports = {
 	hoursCheck,
 	leaveBeforeStart,
-	workableHours
+	workableHours,
+	familyA
 };
--- babysitter.test.js
+++ babysitter.test.js
@@ -1,4 +1,9 @@
-const { hoursCheck, leaveBeforeStart, workableHours } = require('./babysitter');
+const {
+	hoursCheck,
+	leaveBeforeStart,
+	workableHours,
+	familyA
+} = require('./babysitter');
 
 describe('Checking to make sure times are valid.  Need to be between 5pm and 4am, also leave time cannot be before start time', () => {
 	it('should return false with the start time being to early,', () => {
@@ -29,5 +34,24 @@ describe('Checking to make sure times are valid.  Need to be between 5pm and 4am
 
 	it('should return true with valid working hours', () => {
 		expect(workableHours(7, 3)).toBe(true);
+	});
+});
+
+describe('Checking to see the payment of workin done for family A', () => {
+	it('should return "Please check the hours entered" if the times are not valid', () => {
+		expect(familyA(4, 10)).toBe('Please check the hours entered');
+		expect(familyA(10, 5)).toBe('Please check the hours entered');
+		expect(familyA(3, 12)).toBe('Please check the hours entered');
+	});
+	it('should return 75 for working 5 to 10 and only working early hours', () => {
+		expect(familyA(5, 10)).toBe(75); // 5 hours at $15 an hour = $75
+	});
+
+	it('should return 80 for working 11 to 3 and only working late hours', () => {
+		expect(familyA(11, 3)).toBe(80); // 4 hours at $20 = $80
+	});
+
+	it('should return 120 for working 7 to 2 and working both early and late hours', () => {
+		expect(familyA(7, 2)).toBe(120); // 4 early hours = $60 and 3 late hours = $60
 	});
 });
