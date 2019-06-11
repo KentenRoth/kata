16de305fdd2310225f354c7629c0bf763f9b583c
Kent Roth <kentencroth@yahoo.com>
2019-06-10 20:38:53 -0500

    WIP: refactoring the functions to check valid hours

--- babysitter.js
+++ babysitter.js
@@ -2,25 +2,16 @@
 hours = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4];
 
 // Check hours to make sure they are within range, and times are entered correctly
-const hoursCheck = (start, leave) => {
-	const startTime = hours.slice(0, 10).includes(start);
-	const leaveTime = hours.slice(1, 11).includes(leave);
-
-	return startTime && leaveTime ? true : false;
-};
-
-const leaveBeforeStart = (start, leave) => {
-	const startBefore = hours.indexOf(start);
-	const leaveAfter = hours.indexOf(leave) - 1;
-
-	return startBefore <= leaveAfter ? true : false;
-};
-
-const workableHours = (start, leave) => {
-	return hoursCheck(start, leave) && leaveBeforeStart(start, leave)
+const hoursCheck = (start, leave) =>
+	hours.slice(0, 10).includes(start) && hours.slice(1, 11).includes(leave)
 		? true
 		: false;
-};
+
+const leaveBeforeStart = (start, leave) =>
+	hours.indexOf(start) <= hours.indexOf(leave) - 1 ? true : false;
+
+const workableHours = (start, leave) =>
+	hoursCheck(start, leave) && leaveBeforeStart(start, leave) ? true : false;
 
 module.exports = {
 	hoursCheck,
